c9cb6019df906415771d4aef06589e9590c30671
hoangvu-dot <141603999+hoangvu-dot@users.noreply.github.com>
2024-12-07 16:31:42 +0700

    Clear redundant files

--- dashboard.py
+++ dashboard.py
@@ -4,9 +4,10 @@ import json
 from datetime import timedelta , datetime
 import sqlite3, json
 
+
 markers = []
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stalkinator.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ballstorage.db'
 app.permanent_session_lifetime = timedelta(minutes= 5)
 app.config['SECRET_KEY'] = 'averysecretkey'
 db = SQLAlchemy(app)
@@ -14,25 +15,62 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-    def __init__(self, email,password, tid):
+    date = db.Column(db.String(100))
+    def __init__(self, email, password, date = None):
         self.email = email
-        self.tid = tid
         self.password = password
-        
+        self.date = date if date else datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+class Ball(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sport = db.Column(db.String(50), nullable = False)
+    condition = db.Column(db.String(10), nullable = False)
+    def __init__(self, sport, condition):
+        self.sport = sport
+        self.condition = condition
+
     
 @app.route("/" , methods = ["POST", "GET"])
 @app.route("/login" , methods = ["POST", "GET"])
 def login():
-    return render_template("base.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+    
+        session.permanent = True
+        session["email"] = email
+        found_user = User.query.filter_by(email = email).first()
+        
+        if found_user:
+            
+            if password == found_user.password:
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Please type the correct PASSWORD !")
+                session.pop("email",None)
+                session.pop("pass", None)
+                return render_template("login.html")
+        
+        else:
+            flash("You've not register yet")
+            session.pop("email",None)
+            session.pop("pass", None)
+            return render_template("login.html")
+    
+    else:
+        if "email" in session:
+            flash("Already Logged in")
+            return redirect(url_for("dashboard"))
+        
+    return render_template('login.html')
     
 @app.route("/register" , methods = ["POST", "GET"])
 def register():
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
-        thing_id = request.form["tid"]
         
         found_user = User.query.filter_by(email = email).first()
         if found_user:
@@ -43,31 +81,14 @@ def register():
 
         else:
             flash(f'Successfully registered!')
-            account = User(email, password, thing_id)
+            account = User(email=email, password=password)
             db.session.add(account)
             db.session.commit()
+            flash("User created successfully!")
             return redirect(url_for("login"))
     else:
         return render_template("register.html")
     
-
-@app.route("/data", methods=["POST", "GET"])
-def data():
-    
-    connect = sqlite3.connect("instance/stalkinator.db")
-    connect.row_factory = sqlite3.Row  # Set the row_factory to sqlite3.Row
-    cur = connect.cursor()
-    
-    # This handles requests to update safe zone
-    if request.method == "POST" and all(isinstance(float(request.form.get(key)), float) for key in ["lat", "lon", "safeRange"]):
-        ...
-       
-    # This handles periodical requests from Front-end to display the saved coords, and send emails
-    elif request.method == "GET":
-        ...
-        # Save safeZone to database
-
-    return "OK\n"
 
 if __name__ == "__main__":          
     with app.app_context():
