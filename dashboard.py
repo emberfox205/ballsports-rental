f0aec41e12547c68d5699738894c4874c87fd310
Herr Cult <10423041@student.vgu.edu.vn>
2024-12-14 11:21:00 +0700

    added online database view with admin authority

--- dashboard.py
+++ dashboard.py
@@ -379,7 +379,7 @@ def detectReturn():
         
 @app.route('/allData')
 def allData():
-    if "email" in session:
+    if "email" in session and session["email" == "admin123@rentball.com"]:
         connect, cur = connectDb()
         cur.execute("SELECT * FROM ballRent")
         data = cur.fetchall()
